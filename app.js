581d4d4e62a785e42e3417957b4e5ce93777205f
Willie Wrinkle <williewrinkle@gmail.com>
2020-07-22 20:38:49 -0500

    hooks midi to fret ui

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const STRING_RANGES = [{
+  lowest: 64,
+  highest: 79
+}, {
+  lowest: 59,
+  highest: 63
+}, {
+  lowest: 55,
+  highest: 58
+}, {
+  lowest: 50,
+  highest: 54
+}, {
+  lowest: 45,
+  highest: 49
+}, {
+  lowest: 40,
+  highest: 44
+}];
+const strings = document.querySelectorAll('.string');
+
 //check browser for webMIDI compatibility
 if (navigator.requestMIDIAccess) {
     console.log('This browser supports WebMIDI!');
@@ -30,119 +51,57 @@ function getMIDIMessage(midiMessage) {
     console.log(midiMessage);
 };
 
-
 function getMIDIMessage(message) {
-    var command = message.data[0];
-    var note = message.data[1];
-    var velocity = (message.data.length > 2) ? message.data[2] : 0; // a velocity value might not be included with a noteOff command
+  const [command, note, velocity] = message.data;
+  if ((command >= 144 && command <= 150) && velocity > 0) {
+    noteOn(command, note)
+  }
+  else if ((command >= 128 && command <= 134) || velocity === 0) {
+    noteOff(command, note);
+  }
+}
 
-    switch (command) {
-        case 144: // noteOn
-            if (velocity > 0) {
-                noteOn(note, velocity);
-            } else {
-                noteOff(note);
-            }
-            break;
-        case 128: // noteOff
-            noteOff(note);
-            break;
-    };
- 
+function getFret(command, note) {
+  // Too Low
+  if (note < 40) {
+    return;
+  }
 
-  function noteOn(note){
-    if(note = 60) {
-        oneZero.style['opacity'] = 1;
-  };
-    };
-    
-  };
+  let stringIndex;
+  if ([144, 128].includes(command)) {
+    stringIndex = STRING_RANGES.findIndex(string => note >= string.lowest && note <= string.highest);
+  }
+  else if (command >= 129 && command <= 134) {
+    stringIndex = 134 - command;
+  }
+  else if (command >= 145 && command <= 150) {
+    stringIndex = 150 - command;
+  }
 
+  // Command is not right or note is out of range
+  if (stringIndex < 0) {
+    return;
+  }
 
-  function noteOff(note) {
-    
-  };
-  
-  
-//first string notes
-const oneZero = document.getElementById('oneZero');
-oneZero.addEventListener( 'noteOn', function () {
-  if (message.data[1] = 60) {
-  oneZero.style['opacity'] = 1;
-  };
-});
+  const string = strings[stringIndex];
+  const lowestPitch = STRING_RANGES[stringIndex].lowest;
+  const fretIndex = note - lowestPitch;
 
-const oneOne = document.getElementById('oneOne');
-oneOne.addEventListener('click', function () {
-  oneOne.style['opacity'] = 1;
-});
+  return string.querySelectorAll('.note-fret .circle')[fretIndex];
+}
 
-const oneTwo = document.getElementById('oneTwo');
-oneTwo.addEventListener('click', function () {
-  oneTwo.style['opacity'] = 1;
-});
+function setFretOpacity(command, note, opacity) {
+  const fret = getFret(command, note);
+  if (fret) {
+    fret.style.opacity = opacity;
+  }
+}
 
-const oneThree = document.getElementById('oneThree');
-oneThree.addEventListener('click', function () {
-  oneThree.style['opacity'] = 1;
-});
+function noteOn(command, note) {
+  setFretOpacity(command, note, 1);
+}
 
-const oneFour = document.getElementById('oneFour');
-oneFour.addEventListener('click', function () {
-  oneFour.style['opacity'] = 1;
-});
-
-const oneFive = document.getElementById('oneFive');
-oneFive.addEventListener('click', function () {
-  oneFive.style['opacity'] = 1;
-});
-
-const oneSix = document.getElementById('oneSix');
-oneSix.addEventListener('click', function () {
-  oneSix.style['opacity'] = 1;
-});
-
-const oneSeven = document.getElementById('oneSeven');
-oneSeven.addEventListener('click', function () {
-  oneSeven.style['opacity'] = 1;
-});
-
-const oneEight = document.getElementById('oneEight');
-oneEight.addEventListener('click', function () {
-  oneEight.style['opacity'] = 1;
-});
-
-const oneNine = document.getElementById('oneNine');
-oneNine.addEventListener('click', function () {
-  oneNine.style['opacity'] = 1;
-});
-
-const oneTen = document.getElementById('oneTen');
-oneTen.addEventListener('click', function () {
-  oneTen.style['opacity'] = 1;
-});
-
-const oneEleven = document.getElementById('oneEleven');
-oneEleven.addEventListener('click', function () {
-  oneEleven.style['opacity'] = 1;
-});
-const oneTwelve = document.getElementById('oneTwelve');
-oneTwelve.addEventListener('click', function () {
-  oneTwelve.style['opacity'] = 1;
-});
-
-const oneThirteen = document.getElementById('oneThirteen');
-oneThirteen.addEventListener('click', function () {
-  oneThirteen.style['opacity'] = 1;
-});
-
-const oneFourteen = document.getElementById('oneFourteen');
-oneFourteen.addEventListener('click', function () {
-  oneFourteen.style['opacity'] = 1;
-});
-
-const oneFifteen = document.getElementById('oneFifteen');
-oneFifteen.addEventListener('click', function () {
-  oneFifteenfteen.style['opacity'] = 1;
-});
+function noteOff(command, note) {
+  setFretOpacity(command, note, 0);
+}
 
